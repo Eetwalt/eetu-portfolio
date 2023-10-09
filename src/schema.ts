f19d76b2ef4837fc76bdcd680e349e069c9e2e50
Eetwalt <eetu@eeturantanen.fi>
2023-10-10 00:33:46 +0300

    supabase work

--- src/schema.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
-
-export const blogPosts = pgTable('blogPosts', {
-  id: serial('id').primaryKey(),
-  fullName: text('full_name'),
-  phone: varchar('phone', { length: 256 }),
-});
